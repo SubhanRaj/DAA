5f0a2938f8b145dc3e715b450b776a67a4479e69
Subhan Raj <shubhanraj2002@gmail.com>
2022-12-29 14:05:02 +0530

    Implementation of Quick Sort

---
+++ quickSort.c
@@ -0,0 +1,62 @@
+// Implementation of Quick Sort Algorithm in C Language
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int quickSort(int *arr, int left, int right);
+int partition(int *arr, int left, int right);
+void swap(int *a, int *b);
+
+int quickSort(int *arr, int left, int right)
+{
+    if (left < right)
+    {
+        int pivot = partition(arr, left, right);
+        quickSort(arr, left, pivot - 1);
+        quickSort(arr, pivot + 1, right);
+    }
+}
+
+int partition(int *arr, int left, int right)
+{
+    int pivot = arr[right];
+    int i = left - 1;
+    for (int j = left; j < right; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[right]);
+    return i + 1;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int *arr = (int *)malloc(n * sizeof(int));
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    quickSort(arr, 0, n - 1);
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("");
+    return 0;
+}
